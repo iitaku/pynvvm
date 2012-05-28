60f397b5137258f26712e18074a1c8de04d41930
Takuro Iizuka <iitaku@gmail.com>
2012-05-28 12:54:04 +0900

    added nvvm wrapper

--- test.py
+++ test.py
@@ -1,29 +1,91 @@
+import xenodermus.nvvm as xeno
 import xenodermus.gpuarray
 
-xenodermus.gpuarray.map(lambda x : x + 1, [1.0, 2.0, 3.0])
+#xenodermus.gpuarray.map(lambda x : x + 1, [1.0, 2.0, 3.0])
 
-import pycuda.autoinit
-import pycuda.driver as drv
-import numpy
+if __name__ == '__main__':
+  result = xeno.Init()
+  assert xeno.nvvmResult.NVVM_SUCCESS == result.val
+  
+  result, cu = xeno.CreateCU()
+  assert xeno.nvvmResult.NVVM_SUCCESS == result.val
 
-from pycuda.compiler import SourceModule
-mod = SourceModule("""
-__global__ void multiply_them(float *dest, float *a, float *b)
-{
-    const int i = threadIdx.x;
-      dest[i] = a[i] * b[i];
+  code = '''
+target datalayout = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64"
+
+define i32 @ave(i32 %a, i32 %b) {
+entry:
+  %add = add nsw i32 %a, %b
+  %div = sdiv i32 %add, 2
+  ret i32 %div
 }
-""")
 
-multiply_them = mod.get_function("multiply_them")
+define void @simple(i32* %data) {
+entry:
+  %0 = call i32 @llvm.nvvm.read.ptx.sreg.ctaid.x()
+  %1 = call i32 @llvm.nvvm.read.ptx.sreg.ntid.x()
+  %mul = mul i32 %0, %1
+  %2 = call i32 @llvm.nvvm.read.ptx.sreg.tid.x()
+  %add = add i32 %mul, %2
+  %call = call i32 @ave(i32 %add, i32 %add)
+  %idxprom = sext i32 %add to i64
+  %arrayidx = getelementptr inbounds i32* %data, i64 %idxprom
+  store i32 %call, i32* %arrayidx, align 4
+  ret void
+}
 
-a = numpy.random.randn(400).astype(numpy.float32)
-b = numpy.random.randn(400).astype(numpy.float32)
+declare i32 @llvm.nvvm.read.ptx.sreg.ctaid.x() nounwind readnone
 
-dest = numpy.zeros_like(a)
-multiply_them(
-    drv.Out(dest), drv.In(a), drv.In(b),
-    block=(400,1,1), grid=(1,1))
+declare i32 @llvm.nvvm.read.ptx.sreg.ntid.x() nounwind readnone
 
-print dest-a*b
+declare i32 @llvm.nvvm.read.ptx.sreg.tid.x() nounwind readnone
 
+!nvvm.annotations = !{!1}
+!1 = metadata !{void (i32*)* @simple, metadata !"kernel", i32 1}
+  '''
+  
+  result = xeno.CUAddModule(cu, code)
+  assert xeno.nvvmResult.NVVM_SUCCESS == result.val
+ 
+  options = ['-g', '-target=ptx']
+  result = xeno.CompileCU(cu, options)
+  if xeno.nvvmResult.NVVM_SUCCESS != result.val:
+    result, msg_buffer = xeno.GetCompilationLog(cu)
+    print msg_buffer
+    assert xeno.nvvmResult.NVVM_SUCCESS == result.val
+  
+  result, msg_buffer = xeno.GetCompiledResult(cu)
+  print msg_buffer
+  assert xeno.nvvmResult.NVVM_SUCCESS == result.val
+ 
+  result = xeno.DestroyCU(cu)
+  assert xeno.nvvmResult.NVVM_SUCCESS == result.val
+
+  result = xeno.Fini()
+  assert xeno.nvvmResult.NVVM_SUCCESS == result.val
+ 
+#import pycuda.autoinit
+#import pycuda.driver as drv
+#import numpy
+#
+#from pycuda.compiler import SourceModule
+#mod = SourceModule("""
+#__global__ void multiply_them(float *dest, float *a, float *b)
+#{
+#    const int i = threadIdx.x;
+#      dest[i] = a[i] * b[i];
+#}
+#""")
+#
+#multiply_them = mod.get_function("multiply_them")
+#
+#a = numpy.random.randn(400).astype(numpy.float32)
+#b = numpy.random.randn(400).astype(numpy.float32)
+#
+#dest = numpy.zeros_like(a)
+#multiply_them(
+#    drv.Out(dest), drv.In(a), drv.In(b),
+#    block=(400,1,1), grid=(1,1))
+#
+#print dest-a*b
+#
--- xenodermus/nvvm.py
+++ xenodermus/nvvm.py
@@ -15,8 +15,15 @@ class nvvmResult(ctypes.Structure):
    NVVM_ERROR_INVALID_OPTION,
    NVVM_ERROR_NO_MODULE_IN_CU,
    NVVM_ERROR_COMPILATION
-  ) = map(ctypes.c_int, xrange(12))
+  ) = range(12)
   _fields_ = [('val', ctypes.c_int)]
+  
+  def __str__(self):
+    for k, v in nvvmResult.__dict__.items():
+      if 'NVVM' in k:
+        if v == self.val:
+          return k
+    return 'NVVM_UNKNOWN'
 #end nvvmResult
 
 class nvvmCU(ctypes.Structure):
@@ -27,15 +34,15 @@ _this_file = os.path.abspath(__file__)
 _dllfile = os.path.join(os.path.dirname(_this_file), 'LibNVVM/libnvvm.so')
 _dll = ctypes.CDLL(_dllfile)
 
-def init():
+def Init():
   _dll.nvvmInit.restype = nvvmResult
   return _dll.nvvmInit()
 
-def fini():
+def Fini():
   _dll.nvvmFini.restype = nvvmResult
   return _dll.nvvmFini()
 
-def version():
+def Version():
   _dll.nvvmVersion.argtypes = [ctypes.POINTER(ctypes.c_int),
                                ctypes.POINTER(ctypes.c_int)]
   _dll.nvvmVersion.restype = nvvmResult
@@ -44,15 +51,58 @@ def version():
   result = _dll.nvvmVersion(ctypes.byref(major), ctypes.byref(minor))
   return [result, major.value, minor.value]
 
-def createCU():
+def CreateCU():
   _dll.nvvmCreateCU.argtypes = [ctypes.POINTER(nvvmCU)]
   _dll.nvvmCreateCU.restype = nvvmResult
   cu = nvvmCU()
   result = _dll.nvvmCreateCU(ctypes.byref(cu))
   return [result, cu]
 
-def destroyCU(cu):
+def DestroyCU(cu):
   _dll.nvvmDestroyCU.argtypes = [ctypes.POINTER(nvvmCU)]
   _dll.nvvmDestroyCU.restype = nvvmResult
   return _dll.nvvmDestroyCU(ctypes.byref(cu))
+
+def CUAddModule(cu, code):
+  _dll.nvvmCUAddModule.argtypes = [nvvmCU, ctypes.c_char_p, ctypes.c_size_t]
+  _dll.nvvmCUAddModule.restype = nvvmResult
+  return _dll.nvvmCUAddModule(cu, ctypes.c_char_p(code), ctypes.c_size_t(len(code)))
+
+def CompileCU(cu, options):
+  numOptions = len(options)
+  options_t = ctypes.c_char_p * numOptions
+  _dll.nvvmCompileCU.argtypes = [nvvmCU, ctypes.c_int, options_t]
+  _dll.nvvmCompileCU.restype = nvvmResult
+  options_char_p = options_t()
+  for i, s in enumerate(options):
+    options_char_p[i] = ctypes.c_char_p(s)
+  return _dll.nvvmCompileCU(cu, ctypes.c_int(numOptions), options_char_p)
+
+def GetCompiledResult(cu):
+  _dll.nvvmGetCompiledResultSize.argtypes = [nvvmCU, ctypes.POINTER(ctypes.c_size_t)]
+  _dll.nvvmGetCompiledResultSize.restype = nvvmResult
+  size = ctypes.c_size_t()
+  result = _dll.nvvmGetCompiledResultSize(cu, ctypes.byref(size))
+  if nvvmResult.NVVM_SUCCESS != result.val:
+    return result, ''
+  buffer_t = ctypes.c_char * size.value
+  _dll.nvvmGetCompiledResult.argtypes = [nvvmCU, buffer_t]
+  _dll.nvvmGetCompiledResult.restype = nvvmResult
+  msg_buffer = buffer_t()
+  result = _dll.nvvmGetCompiledResult(cu, msg_buffer)
+  return result, msg_buffer.value
+
+def GetCompilationLog(cu):
+  _dll.nvvmGetCompilationLogSize.argtypes = [nvvmCU, ctypes.POINTER(ctypes.c_size_t)]
+  _dll.nvvmGetCompilationLogSize.restype = nvvmResult
+  size = ctypes.c_size_t()
+  result = _dll.nvvmGetCompilationLogSize(cu, ctypes.byref(size))
+  if nvvmResult.NVVM_SUCCESS != result.val:
+    return result, ''
+  buffer_t = ctypes.c_char * size.value
+  _dll.nvvmGetCompilationLog.argtypes = [nvvmCU, buffer_t]
+  _dll.nvvmGetCompilationLog.restype = nvvmResult
+  msg_buffer = buffer_t()
+  result = _dll.nvvmGetCompilationLog(cu, msg_buffer)
+  return result, msg_buffer.value
 
