ff4ddf40fc9a7b003ff0beda51e2482aad911aba
Takuro Iizuka <iitaku@gmail.com>
2012-05-30 08:42:26 +0900

    fixed test.py

--- test.py
+++ test.py
@@ -1,38 +1,7 @@
-<<<<<<< local
-#import math
-#from xenodermus import Composer, Mapper, Reducer
-#
-#def main():
-#  m = Mapper(lambda x : math.sqrt(x + 1))
-#  r = Reducer(lambda x, y : x + y)
-#  c = Composer([m, r])
-#  result = c.run([1.0, 2.0, 3.0])
-#  print(result)
-#
-#if __name__ == '__main__':
-#  main()
-  
-import pycuda.autoinit
-import pycuda.driver as drv
-=======
 import math
->>>>>>> other
 import numpy
-<<<<<<< local
-=======
 from xenodermus import Composer, Mapper, Reducer
->>>>>>> other
 
-<<<<<<< local
-from pycuda.compiler import SourceModule
-mod = SourceModule("""
-__global__ void multiply_them(float *dest, float *a, float *b)
-{
-    const int i = threadIdx.x;
-      dest[i] = a[i] * b[i];
-}
-""")
-=======
 def main():
   #m = Mapper(lambda x : math.sqrt(x + 1))
   m = Mapper(lambda x : math.sqrt(x + 1))
@@ -43,11 +12,7 @@ def main():
   outgoing = c.run(incoming)
   print(incoming)
   print(outgoing)
->>>>>>> other
 
-<<<<<<< local
-multiply_them = mod.get_function("multiply_them")
-=======
 if __name__ == '__main__':
   main()
   
@@ -76,15 +41,13 @@ if __name__ == '__main__':
 ##    block=(400,1,1), grid=(1,1))
 ##
 ##print(dest-a*b)
->>>>>>> other
-
-a = numpy.random.randn(400).astype(numpy.float32)
-b = numpy.random.randn(400).astype(numpy.float32)
-
-dest = numpy.zeros_like(a)
-multiply_them(
-    drv.Out(dest), drv.In(a), drv.In(b),
-    block=(400,1,1), grid=(1,1))
-
-print(dest-a*b)
-
+#a = numpy.random.randn(400).astype(numpy.float32)
+#b = numpy.random.randn(400).astype(numpy.float32)
+#
+#dest = numpy.zeros_like(a)
+#multiply_them(
+#    drv.Out(dest), drv.In(a), drv.In(b),
+#    block=(400,1,1), grid=(1,1))
+#
+#print(dest-a*b)
+#
