6859fedabb02d7b127fda8466f148f9fc59ddb37
Takuro Iizuka <iitaku@gmail.com>
2012-05-29 19:14:40 +0900

    added some AST traverser

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import math
+import numpy
 from xenodermus import Composer, Mapper, Reducer
 
 def main():
+  #m = Mapper(lambda x : math.sqrt(x + 1))
   m = Mapper(lambda x : math.sqrt(x + 1))
-  r = Reducer(lambda x, y : x + y)
+  r = Reducer(lambda x, y : x if x < y else y)
   c = Composer([m, r])
-  result = c.run([1.0, 2.0, 3.0])
-  print(result)
+  
+  incoming = numpy.array([1.0, 2.0, 3.0]).astype(numpy.float32)
+  outgoing = c.run(incoming)
+  print(incoming)
+  print(outgoing)
 
 if __name__ == '__main__':
   main()
@@ -16,23 +21,24 @@ if __name__ == '__main__':
 #import numpy
 #
 #from pycuda.compiler import SourceModule
-#mod = SourceModule("""
-#__global__ void multiply_them(float *dest, float *a, float *b)
-#{
-#    const int i = threadIdx.x;
-#      dest[i] = a[i] * b[i];
-#}
-#""")
 #
-#multiply_them = mod.get_function("multiply_them")
-#
-#a = numpy.random.randn(400).astype(numpy.float32)
-#b = numpy.random.randn(400).astype(numpy.float32)
-#
-#dest = numpy.zeros_like(a)
-#multiply_them(
-#    drv.Out(dest), drv.In(a), drv.In(b),
-#    block=(400,1,1), grid=(1,1))
-#
-#print(dest-a*b)
+##mod = SourceModule("""
+##__global__ void multiply_them(float *dest, float *a, float *b)
+##{
+##    const int i = threadIdx.x;
+##      dest[i] = a[i] * b[i];
+##}
+##""")
+##
+##multiply_them = mod.get_function("multiply_them")
+##
+##a = numpy.random.randn(400).astype(numpy.float32)
+##b = numpy.random.randn(400).astype(numpy.float32)
+##
+##dest = numpy.zeros_like(a)
+##multiply_them(
+##    drv.Out(dest), drv.In(a), drv.In(b),
+##    block=(400,1,1), grid=(1,1))
+##
+##print(dest-a*b)
 
--- xenodermus/sequencer.py
+++ xenodermus/sequencer.py
@@ -1,5 +1,7 @@
 import ast
 import inspect
+import sys
+from string import Template
 
 import pycuda.autoinit
 import pycuda.driver as drv
@@ -8,30 +10,117 @@ import numpy
 from . import nvvm
 
 class ASTTraverser(ast.NodeVisitor):
+  
   def generic_visit(self, node):
-    print(' : '.join([type(node).__name__, str(node.__dict__)]))
+    print('%10s : %s' % (type(node).__name__, str(node.__dict__)))
     ast.NodeVisitor.generic_visit(self, node)
+
 #end class ASTTraverser
 
+
 class LambdaExtractor(ast.NodeVisitor):
+
+  def __init__(self):
+    self._lambda_node = None
+  
   def generic_visit(self, node):
-    if ast.Lambda == type(node):
+    ast.NodeVisitor.generic_visit(self, node)
+  
+  def visit_Lambda(self, node):
+    if None == self._lambda_node:
       self._lambda_node = node
-      return
-    else:
       ast.NodeVisitor.generic_visit(self, node)
+    else:
+      raise Exception, 'error : lambda expr contain another lambda expr'
+  
   @property
   def lambda_node(self):
     return self._lambda_node
+
 #end class LambdaExtractor
 
+
+class MapLambdaTraverser(ast.NodeVisitor):
+   
+  def generic_visit(self, node):
+    ast.NodeVisitor.generic_visit(self, node)
+
+   
+#end class MapLambdaTraverser
+
+
 class Composer:
+  
   def __init__(self, sequences):
+    self._layout = '''
+    target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64"
+    '''
+    
+    self._intrinsics = '''
+    declare float  @llvm.sqrt.f32(float)  nounwind readnone
+    declare double @llvm.sqrt.f64(double) nounwind readnone
+    
+    declare float  @llvm.fma.f32(float)  nounwind readnone
+    declare double @llvm.fma.f64(double) nounwind readnone
+    
+    declare i16 @llvm.bswap.i16(i16) nounwind readnone
+    declare i32 @llvm.bswap.i32(i32) nounwind readnone
+    declare i64 @llvm.bswap.f64(i64) nounwind readnone
+    
+    declare i8  @llvm.ctpop.i8(i8)   nounwind readnone
+    declare i16 @llvm.ctpop.i16(i16) nounwind readnone
+    declare i32 @llvm.ctpop.i32(i32) nounwind readnone
+    declare i64 @llvm.ctpop.f64(i64) nounwind readnone
+      
+    declare i8  @llvm.ctlz.i8(i8)   nounwind readnone
+    declare i16 @llvm.ctlz.i16(i16) nounwind readnone
+    declare i32 @llvm.ctlz.i32(i32) nounwind readnone
+    declare i64 @llvm.ctlz.f64(i64) nounwind readnone
+
+    declare i8  @llvm.cttz.i8(i8)   nounwind readnone
+    declare i16 @llvm.cttz.i16(i16) nounwind readnone
+    declare i32 @llvm.cttz.i32(i32) nounwind readnone
+    declare i64 @llvm.cttz.f64(i64) nounwind readnone
+
+    declare i32 @llvm.nvvm.read.ptx.sreg.ctaid.x() nounwind readnone
+    declare i32 @llvm.nvvm.read.ptx.sreg.ntid.x() nounwind readnone
+    declare i32 @llvm.nvvm.read.ptx.sreg.tid.x() nounwind readnone
+    '''
+    
+    self._kernel ='''
+    define void @stub_kernel(${type}* %incoming, ${type}* %outgoing) {
+    entry:  
+      %0 = call i32 @llvm.nvvm.read.ptx.sreg.ctaid.x()
+      %1 = call i32 @llvm.nvvm.read.ptx.sreg.ntid.x()
+      %2 = mul i32 %0, %1
+      %3 = call i32 @llvm.nvvm.read.ptx.sreg.tid.x()
+      %id = add i32 %2, %3
+      %idx = sext i32 %id to i64
+      %incoming_p = getelementptr inbounds ${type}* %incoming, i64 %idx
+      %cmp = icmp slt i64 %idx, ${num}
+      br i1 %cmp, label %cont, label %exit
+    cont:
+      ${logic}
+      br label %exit
+    exit:
+      ret void
+    }
+    '''
+    
+    self._metadata = '''
+    !nvvm.annotations = !{!1}
+    !1 = metadata !{void (${type}*, ${type}*)* @stub_kernel, metadata !"kernel", i32 1}
+    '''
+    
     result, self._cu = nvvm.instance.create_cu()
     assert nvvm.nvvmResult.NVVM_SUCCESS == result.val
     
+    self._func = ''
+    
+    self._sequences = sequences
+    
     for s in sequences:
-      self._codegen(s)
+      self._func += s.codegen() + '\n'
     return
  
   def __del__(self):
@@ -39,70 +128,50 @@ class Composer:
     assert nvvm.nvvmResult.NVVM_SUCCESS == result.val
     return
 
-  def _codegen(self, s):
-    return
+  def run(self, incoming):
+       
+    code_template = Template('\n'.join([self._layout, self._func, self._kernel, self._intrinsics, self._metadata]))
 
-  def run(self, array):
- 
-    code = '''
-target datalayout = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-v128:128:128-n16:32:64"
+    print(incoming.dtype)
+    
+    test_type = 'float'
+    test_logic = '''
+    %outgoing_p = getelementptr inbounds float* %outgoing, i64 %idx
+    %res = call float @llvm.sqrt.f32(float 2514.0)
+    store float %res, float* %outgoing_p, align 4
+    '''
+    
+    code = code_template.safe_substitute({'type':test_type, 'num':len(incoming), 'logic':test_logic})
+    
+    #for i, line in enumerate(code.strip().split('\n')):
+    #  print('%3d : %s' % (i+1, line)) 
 
-define i32 @ave(i32 %a, i32 %b) {
-entry:
-  %add = add nsw i32 %a, %b
-  %div = sdiv i32 %add, 2
-  ret i32 %div
-}
-
-define void @simple(i32* %data) {
-entry:
-  %0 = call i32 @llvm.nvvm.read.ptx.sreg.ctaid.x()
-  %1 = call i32 @llvm.nvvm.read.ptx.sreg.ntid.x()
-  %mul = mul i32 %0, %1
-  %2 = call i32 @llvm.nvvm.read.ptx.sreg.tid.x()
-  %add = add i32 %mul, %2
-  %call = call i32 @ave(i32 %add, i32 %add)
-  %idxprom = sext i32 %add to i64
-  %arrayidx = getelementptr inbounds i32* %data, i64 %idxprom
-  store i32 %call, i32* %arrayidx, align 4
-  ret void
-}
-
-declare i32 @llvm.nvvm.read.ptx.sreg.ctaid.x() nounwind readnone
-
-declare i32 @llvm.nvvm.read.ptx.sreg.ntid.x() nounwind readnone
-
-declare i32 @llvm.nvvm.read.ptx.sreg.tid.x() nounwind readnone
-
-!nvvm.annotations = !{!1}
-!1 = metadata !{void (i32*)* @simple, metadata !"kernel", i32 1}
-  '''
-  
     result = nvvm.instance.cu_add_module(self._cu, code)
     assert nvvm.nvvmResult.NVVM_SUCCESS == result.val
  
-    options = ['-target=ptx']
+    options = []
     result = nvvm.instance.compile_cu(self._cu, options)
     if nvvm.nvvmResult.NVVM_SUCCESS != result.val:
       result, msg_buffer = nvvm.instance.get_compilation_log(self._cu)
       print(msg_buffer)
       assert nvvm.nvvmResult.NVVM_SUCCESS == result.val
+      sys.exit(-1)
     
     result, ptx_code = nvvm.instance.get_compiled_result(self._cu)
-    print(ptx_code)
+    #print(ptx_code)
     assert nvvm.nvvmResult.NVVM_SUCCESS == result.val
-
+    
     m = drv.module_from_buffer(ptx_code)
-    simple = m.get_function('simple')
-
-    print simple.__dict__
+    stub_kernel = m.get_function('stub_kernel')
    
-    data = numpy.random.randn(16).astype(numpy.int32)
-    print data
-    simple(drv.InOut(data), block=(16, 1, 1), grid=(1, 1))
-    print data
+    block_size = (256, 1, 1)
+    grid_size  = (1024, 1, 1)
 
-    return array
+    outgoing = numpy.arange(len(incoming)).astype(numpy.float32)
+  
+    stub_kernel(drv.In(incoming), drv.Out(outgoing), block=block_size, grid=grid_size)
+
+    return outgoing
 #end class Composer
 
 class Sequencer:
@@ -122,9 +191,38 @@ class Sequencer:
 class Mapper(Sequencer):
   def __init__(self, fun):
     Sequencer.__init__(self, fun)
-    #ASTTraverser().visit(self._lambda_node)
+    ASTTraverser().visit(self._lambda_node)
     #print('')
+    if not 1 == len(self._lambda_node.args.args):
+      raise Exception, 'error : lambda expr must accept #1 args'
     return
+
+  def codegen(self):
+    class Visitor(ast.NodeVisitor):
+      def __init__(self):
+        self.fun = ''
+        return
+      
+      def visit_Lambda(self, node):
+        self.fun += Template('''
+        define ${type} @${fun} (${type} %${val}) {
+        entry:
+        ${local_logic}
+        ret ${type} %res
+        }
+        ''').safe_substitute({'val':node.args.args[0].id})
+        
+        ast.NodeVisitor.generic_visit(self, node)
+            
+      def visit_Call(self, node):
+         print(help(node))
+    
+    #end class Visitor
+
+    v = Visitor()
+    v.visit(self._lambda_node)
+    
+    return v.fun
 #end class Mapper
 
 class Reducer(Sequencer):
@@ -133,5 +231,8 @@ class Reducer(Sequencer):
     #ASTTraverser().visit(self._lambda_node)
     #print('')
     return
+  
+  def codegen(self):
+    return ''
 #end class Mapper
 
